3a35e50f6478f2d2268d318b8066d2a7c2c32781
Hugo Gu <hugogu@outlook.com>
2012-03-21 22:28:31 +0800

    Add Performance Test for the property accessor

---
+++ trunk/Opds4Net/Opds4Net.Test/Common/TestTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Opds4Net.Test.Common
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class TestTimer
+    {
+        private Action action;
+        private Stopwatch timer = new Stopwatch();
+
+        public TestTimer(Action action)
+        {
+            if (action == null)
+                throw new ArgumentNullException("action");
+
+            this.action = action;
+        }
+
+        public TimeSpan Run(int times)
+        {
+            // Run a time to worm up.
+            action();
+
+            timer.Restart();
+            int counter = 0;
+            while (counter++ < times)
+            {
+                action();
+            }
+            timer.Stop();
+
+            return timer.Elapsed;
+        }
+    }
+}
--- trunk/Opds4Net/Opds4Net.Test/ModelHelperTest.cs
+++ trunk/Opds4Net/Opds4Net.Test/ModelHelperTest.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Opds4Net.Test.Common;
 using Opds4Net.Test.Model;
 using Opds4Net.Util;
 using Opds4Net.Util.Extension;
@@ -39,6 +41,43 @@ namespace Opds4Net.Test
 
             Assert.AreEqual(name, model.GetProperty("Name"));
             Assert.AreEqual(name, model.GetProperty("Title"));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void GetPropertyPerformanceTest()
+        {
+            var time = 10000000;
+            var name = "Dummy";
+            var model = new DataModel() { Name = name };
+            var classSpecifiedAccessor = model.GetPropertyAccessor();
+            var globalTarget = String.Empty;
+
+            var timeDynamic = new TestTimer(() =>
+            {
+                globalTarget = classSpecifiedAccessor.GetProperty(model, "Name").ToNullableString();
+            }).Run(time);
+            Assert.AreEqual(name, globalTarget);
+
+            var timeStatic = new TestTimer(() =>
+            {
+                globalTarget = model.Name;
+            }).Run(time);
+            Assert.AreEqual(name, globalTarget);
+
+            var timeRandomClass = new TestTimer(() =>
+            {
+                globalTarget = model.GetProperty("Name").ToNullableString();
+            }).Run(time);
+            Assert.AreEqual(name, globalTarget);
+
+            Assert.IsTrue(timeDynamic > timeStatic);
+            Assert.IsTrue(timeRandomClass > timeDynamic);
+
+            Assert.IsTrue(timeDynamic.TotalMilliseconds < timeStatic.TotalMilliseconds * 10);
+            Assert.IsTrue(timeRandomClass.TotalMilliseconds < timeStatic.TotalMilliseconds * 15);
         }
     }
 }
