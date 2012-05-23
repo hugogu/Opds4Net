d59eb4bc892039260eba5cb607c873d3459a1b5d
Hugo Gu <hugogu@outlook.com>
2012-05-23 16:00:23 +0800

    Add a test for xml text escaping. Syndication escape more characters than xml writer which is not my expecting behavior. And it is hard to workaround. For example, the character { , is valid in xml and url, but syndication feed escape it automatically. The open search protocal use that grammar in url, http://company.org/search?k={searchterms}

--- trunk/Opds4Net/Opds4Net.Test/OpdsValidationTest.cs
+++ trunk/Opds4Net/Opds4Net.Test/OpdsValidationTest.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Diagnostics;
+using System.IO;
+using System.ServiceModel.Syndication;
+using System.Text;
 using System.Xml;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Opds4Net.Model.Validation;
@@ -46,6 +49,39 @@ namespace Opds4Net.Test
             Trace.TraceInformation(String.Format("Schema File Validated, {0} ms used.", watch.ElapsedMilliseconds));
 
             Assert.AreEqual(1, errors);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        [TestMethod]
+        public void CharacterEscapingTest()
+        {
+            var xmlBuilder = new MemoryStream();
+            var writer = new XmlTextWriter(xmlBuilder, Encoding.UTF8);
+            writer.WriteStartDocument();
+            writer.WriteStartElement("doc");
+            writer.WriteAttributeString("attr", "http://www.baidu.com/get?id=5&{}");
+            writer.WriteEndElement();
+            writer.WriteEndDocument();
+            writer.Flush();
+            xmlBuilder.Seek(0, SeekOrigin.Begin);
+            var result = new StreamReader(xmlBuilder).ReadToEnd();
+            // xml Writer don't escape {} 
+            Assert.IsTrue(result.Contains("{}"));
+
+            xmlBuilder = new MemoryStream();
+            writer = new XmlTextWriter(xmlBuilder, Encoding.UTF8);
+            SyndicationItem item = new SyndicationItem("下载", "", new Uri("http://www.baidu.com/get?id=5&{}"));
+            SyndicationFeed feed = new SyndicationFeed(new [] { item });
+            feed.SaveAsAtom10(writer);
+            writer.Flush();
+            xmlBuilder.Seek(0, SeekOrigin.Begin);
+            result = new StreamReader(xmlBuilder).ReadToEnd();
+
+            // feed Writer escape {} to %7B%7D, fuck! Why? 
+            // {} is valid in Atom and Url.
+            Assert.IsFalse(result.Contains("{}"));
         }
     }
 }
